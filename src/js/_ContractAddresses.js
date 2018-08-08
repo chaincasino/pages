fd64b5c4a53f6734cee96dba8b528d6dca6d53e6
lezi2012@qq.com <lezi2012@qq.com>
2018-08-08 18:27:02 +0800

    cc

--- src/js/_ContractAddresses.js
+++ src/js/_ContractAddresses.js
@@ -1,8 +1,49 @@
-let ContractAddresses = {
-    BonusDivestServiceImpl: '0x6312e4332f0c30d604fdde91e470d350b26255a2',
-    BonusServiceImpl: '0x40160ddb05bb97ab04802f8470113ff24d92b911',
-    CommonStorage: '0x21705d84182b45bbf4be8a61a1691300232b3139',
-    Migrations: '0x2b2929aaa8542f6dc7f1cf9d37340734b3893c44',
-    StandardToken: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
-    TokenServiceImpl: '0xd6ea6f5a0ab768321b3585902e89a2b2a5ead6d1'
+let NETS = {
+    main: {name: "MAIN", test: false},
+    reposten: {name: "ROPSTEN TEST NET", test: true},
+    kovan: {name: "KOVAN TEST NET", test: true},
+    rinkeby: {name: "RINKEBY TEST NET", test: true},
 };
+
+
+let ContractAddressesOfNets = {
+    main: {
+        BonusDivestServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        BonusServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        CommonStorage: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        ControlServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        PeriodJob: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        StandardToken: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        TokenServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+    }
+    , ropsten: {
+        BonusDivestServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        BonusServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        CommonStorage: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        ControlServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        PeriodJob: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        StandardToken: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        TokenServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+    }
+    , kovan: {
+        BonusDivestServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        BonusServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        CommonStorage: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        ControlServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        PeriodJob: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        StandardToken: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        TokenServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+    }
+    , rinkeby: {
+        BonusDivestServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        BonusServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        CommonStorage: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        ControlServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        PeriodJob: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        StandardToken: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+        TokenServiceImpl: '0x56c71b77416b097fec1aec314e12b89ea4684e36',
+    }
+};
+
+
+let ContractAddresses = ContractAddressesOfNets.main;
